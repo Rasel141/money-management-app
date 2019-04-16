7fa4aa15c4b6fe3bc35caab20234569ba69de80e
Rasel <rrasel141@gmail.com>
2019-04-17 01:15:39 +0600

    Add - all dependencies & mongodb & Connect frontend -backend in same port

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+// call express
+const app = express()
+
+// call morgan middleware for checking what types of request
+app.use(morgan('dev'))
+
+// call cors for server blocking issue
+app.use(cors())
+
+// use body-parser for getting request body
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Create Route
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to our project'
+  })
+})
+
+// Create Port
+PORT = process.env.PORT || 4000
+
+// Listen Server
+app.listen(PORT, () => {
+  console.log(`SERVER IS RUNNING ON PORT ${PORT}`)
+  mongoose.connect(
+    'mongodb://localhost/money-management-app',
+    { useNewUrlParser: true },
+    () => {
+      console.log('Database Connected')
+    }
+  )
+})
