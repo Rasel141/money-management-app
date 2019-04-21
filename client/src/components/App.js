d2bc711ddfe3308fb22af0c65a2c09dab5d3fafd
Rasel <rrasel141@gmail.com>
2019-04-21 17:53:01 +0600

    Register Functionality done

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           <Switch>
             <Route path='/' exact component={Home} />
             <Route path='/login' component={Login} />
-            <Route path='register' component={Register} />
+            <Route path='/register' component={Register} />
           </Switch>
         </div>
       </BrowserRouter>
